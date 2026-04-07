a29d30e94c6eb83f2307e8d414823a8c734aa051
Rudston <rudston10@gmail.com>
2026-04-07 19:18:34 -0300

    Cadastro com CPF e CNPJ funcionando com mascara e validacoes

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -6,33 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
         cpfInput.addEventListener("input", function (e) {
             e.target.value = formatCPF(e.target.value);
         });
+
+        cpfInput.value = formatCPF(cpfInput.value);
     }
 
     if (cnpjInput) {
         cnpjInput.addEventListener("input", function (e) {
             e.target.value = formatCNPJ(e.target.value);
         });
+
+        cnpjInput.value = formatCNPJ(cnpjInput.value);
     }
 });
 
 function onlyDigits(value) {
-    return value.replace(/\D/g, "");
+    return (value || "").replace(/\D/g, "");
 }
 
 function formatCPF(value) {
     value = onlyDigits(value).slice(0, 11);
 
-    if (value.length <= 3) {
-        return value;
-    }
-
-    if (value.length <= 6) {
-        return value.replace(/^(\d{3})(\d+)/, "$1.$2");
-    }
-
-    if (value.length <= 9) {
-        return value.replace(/^(\d{3})(\d{3})(\d+)/, "$1.$2.$3");
-    }
+    if (value.length <= 3) return value;
+    if (value.length <= 6) return value.replace(/^(\d{3})(\d+)/, "$1.$2");
+    if (value.length <= 9) return value.replace(/^(\d{3})(\d{3})(\d+)/, "$1.$2.$3");
 
     return value.replace(/^(\d{3})(\d{3})(\d{3})(\d{1,2}).*/, "$1.$2.$3-$4");
 }
@@ -40,21 +36,10 @@ function formatCPF(value) {
 function formatCNPJ(value) {
     value = onlyDigits(value).slice(0, 14);
 
-    if (value.length <= 2) {
-        return value;
-    }
-
-    if (value.length <= 5) {
-        return value.replace(/^(\d{2})(\d+)/, "$1.$2");
-    }
-
-    if (value.length <= 8) {
-        return value.replace(/^(\d{2})(\d{3})(\d+)/, "$1.$2.$3");
-    }
-
-    if (value.length <= 12) {
-        return value.replace(/^(\d{2})(\d{3})(\d{3})(\d+)/, "$1.$2.$3/$4");
-    }
+    if (value.length <= 2) return value;
+    if (value.length <= 5) return value.replace(/^(\d{2})(\d+)/, "$1.$2");
+    if (value.length <= 8) return value.replace(/^(\d{2})(\d{3})(\d+)/, "$1.$2.$3");
+    if (value.length <= 12) return value.replace(/^(\d{2})(\d{3})(\d{3})(\d+)/, "$1.$2.$3/$4");
 
     return value.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{1,2}).*/, "$1.$2.$3/$4-$5");
 }
