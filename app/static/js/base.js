7b6eaa0388bb1f68ae64f2b3e5d4deb00ffd6737
Rudston <rudston10@gmail.com>
2026-04-09 22:40:40 -0300

    fix(js): corrigido base.js com máscaras e alerta funcionando corretamente

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -24,6 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
         input.value = formatPhone(input.value);
     });
 
+    const alerts = document.querySelectorAll(".app-alert");
+
+    alerts.forEach((alert) => {
+        setTimeout(() => {
+            alert.style.transition = "opacity 0.4s ease, transform 0.4s ease";
+            alert.style.opacity = "0";
+            alert.style.transform = "translateY(-8px)";
+
+            setTimeout(() => {
+                alert.remove();
+            }, 400);
+        }, 1800);
+    });
+
 });
 
 
